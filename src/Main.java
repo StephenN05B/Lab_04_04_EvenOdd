12995cdd46bf82dd60009a9c11a579ec3736b357
steph <stephenbenny050@gmail.com>
2024-09-19 19:06:26 -0400

    Completed Part 4

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+       double number = 8;
+
+        if (number % 2 == 0) {
+            System.out.println("The number is even");
+        } else {
+            System.out.println("The number is odd");
+        }
+    }
+}
